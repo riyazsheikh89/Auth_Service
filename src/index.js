a1627ea53ecee12917de4619445e4428fa327f74
Sheikh Riyaz Ahmed <wbriyaz89@gmail.com>
2023-01-07 15:50:13 +0530

    added JWT creation and validation logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {PORT} = require('./config/serverConfig');
+const {PORT, JWT_KEY} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 const app = express();
 
-// const { User } = require('./models/index');
-// const bcrypt = require('bcrypt');
-// const UserRepository = require('./repository/user-repository');
+const UserService = require('./services/user-service');
 
 const preapareAndStartServer = () => {
 
@@ -16,15 +14,7 @@ const preapareAndStartServer = () => {
 
     app.listen(PORT, async () => {
         console.log(`Server started on: ${PORT}`);
-
-        // const obj = new UserRepository();
-        // const response = await obj.getById(1);
-        // console.log(response);
-
-        /* const incomingPass = "pass34546";
-        const user = await User.findByPk(5);
-        const response = bcrypt.compareSync(incomingPass, user.password);
-        console.log(response);  */
+        
     });
 }
 
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+const {JWT_KEY} = require('../config/serverConfig');
 const UserRepository = require('../repository/user-repository');
 
 class UserService {
@@ -11,6 +13,27 @@ class UserService {
             return user;
         } catch (error) {
             console.log("Something went wrong at service layer!");
+            throw error;
+        }
+    }
+
+    createToken(user) {
+        try {
+            const result = jwt.sign(user, JWT_KEY, {expiresIn: '1d'});
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in token creation!");
+            throw(error);
+        }
+    }
+
+    verifyToken(token) {
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in token validation!");
+            console.log(error);
             throw error;
         }
     }
